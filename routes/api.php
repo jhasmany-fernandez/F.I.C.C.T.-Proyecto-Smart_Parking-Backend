ad74ad9f92d38157c0ffcf3eef28b78806a3a340
yordi54 <yordice75@gmail.com>
2023-07-10 17:56:52 -0400

    anadir servicio barra vehicular

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
-
+use App\Http\Controllers\MqttController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/messageMqtt/{parametro}', [MqttController::class, 'publishMessage']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
