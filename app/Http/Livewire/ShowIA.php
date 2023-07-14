9350c2ee2c9f5da3f328eb68751a2cf0a4baa778
Fernando <jchilelaime38@gmail.com>
2023-07-14 03:59:14 -0400

    cambios en la verficiacion del espacio

--- app/Http/Livewire/ShowIA.php
+++ app/Http/Livewire/ShowIA.php
@@ -187,6 +187,29 @@ class ShowIA extends Component
         $result = $output;
 
         dd($result);
+        $numero = intval($result);
+        if($numero!=1){
+            $url = 'https://fcm.googleapis.com/fcm/send';
+            $body = [
+                'notification' => [
+                    'body' => '¡Se solicita que estacione en el lugar de reserva que usted eligió!',
+                    'title' => 'Nos hemos dado cuenta que ha parqueado mal su vehículo, estacionelo en el lugar correcto.'
+                ],
+                'to' => 'e7TtAb_tQVejao8XOwnqUL:APA91bEXLJKNIkHfFRepxx06EBuGObu7tlFbo0PSxrRTX2CtYrFeTnkbetvcSfUI7cQZZPq7NQsIfEdT8czEfYmsDj9Rb3tfJ0F4fKVBNTlpi9NLIzor4vi-k5OShTyCimQmyqf3tgff'
+            ];
+            $header = [
+                'Authorization' => 'key=AAAA9TOXkd8:APA91bHncyaTpNAmanNpegOVaWGBNXCSIxjP0jp3J01iiY0qhfPSbQa_LGdxKmNK-TvO1yXGbcftakL9ZkcW0gamEO8xIFMPQie_ay5yZHItaJZZ60tEGFWbeCklOiTWJkVRFfqvT8fz'
+            ];
+
+            $response = Http::withHeaders($header)->post($url, $body);
+
+            // Manejar la respuesta de la solicitud
+            if ($response->successful()) {
+                return response()->json(['message' => 'Notificación enviada correctamente']);
+            } else {
+                return response()->json(['message' => 'Error al enviar la notificación'], 500);
+            }
+        }
     
     }
 
